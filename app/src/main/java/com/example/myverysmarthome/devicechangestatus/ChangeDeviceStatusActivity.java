92d424c232d7d48195f322aad7b76d48892ca98b
jucic1 <julia.cicha60@gmail.com>
2021-07-05 15:27:27 +0200

    added widgets, styled change device status

--- app/src/main/java/com/example/myverysmarthome/devicechangestatus/ChangeDeviceStatusActivity.java
+++ app/src/main/java/com/example/myverysmarthome/devicechangestatus/ChangeDeviceStatusActivity.java
@@ -2,15 +2,23 @@ package com.example.myverysmarthome.devicechangestatus;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.SwitchCompat;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.myverysmarthome.DataContainer;
 import com.example.myverysmarthome.databinding.ActivityChangeDeviceStatusBinding;
+import com.example.myverysmarthome.model.Category;
 import com.example.myverysmarthome.model.Device;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class ChangeDeviceStatusActivity extends AppCompatActivity {
     final static String EXTRA_ITEM_KEY = "extra_item_key";
@@ -52,8 +60,49 @@ public class ChangeDeviceStatusActivity extends AppCompatActivity {
     }
 
     void getElement() {
-       SwitchCompat someSwitch = new SwitchCompat(this);
-       activityChangeDeviceStatusBinding.container.addView(someSwitch,LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+//       SwitchCompat someSwitch = new SwitchCompat(this);
+//       someSwitch.setScaleX(3);
+//       someSwitch.setScaleY(3);
+//       activityChangeDeviceStatusBinding.container.addView(someSwitch,LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+
+//        Slider slider = new Slider(this);
+//        slider.setValueFrom(15);
+//        slider.setValueTo(30);
+//        slider.setValue(20);
+//        slider.setScaleX((float) 0.5);
+//        slider.setScaleY(2);
+//        activityChangeDeviceStatusBinding.container.addView(slider, LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+
+
+        ColorStateList colorStateList = new ColorStateList(
+                new int[][]{
+                        new int[]{-android.R.attr.state_checked},
+                        new int[]{android.R.attr.state_checked}
+                },
+                new int[]{
+                        Color.WHITE, Color.WHITE,
+                }
+        );
+        RadioGroup radioGroup = new RadioGroup(this);
+        radioGroup.setOrientation(RadioGroup.HORIZONTAL);
+        ArrayList<String> options =  new ArrayList<String>( Arrays.asList("Lagodny", "Sredni", "Mocny"));
+        for (String option : options) {
+            RadioButton categoryRadioButton = new RadioButton(this);
+            categoryRadioButton.setTextColor(Color.WHITE);
+            categoryRadioButton.setTextSize(18);
+            categoryRadioButton.setPadding(15, 15, 15, 15);
+            categoryRadioButton.setText(option);
+            categoryRadioButton.setButtonTintList(colorStateList);
+//                categoryRadioButton.setOnClickListener(view -> {
+//                    if (((RadioButton) view).isChecked()) {
+//                        configureDeviceViewModel.setCategory(category);
+//                    }
+//                });
+
+            radioGroup.addView(categoryRadioButton);
+        }
+        activityChangeDeviceStatusBinding.container.addView(radioGroup, LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+
     }
 
 }
